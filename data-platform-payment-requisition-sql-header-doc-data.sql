8b95f7e2adaac746e56ef8c45da517882107e612
Yurino Oga <yurino.o@latona.io>
2022-12-20 09:04:06 +0900

    release

--- data-platform-payment-requisition-sql-header-doc-data.sql
+++ data-platform-payment-requisition-sql-header-doc-data.sql
@@ -1,11 +1,14 @@
-CREATE TABLE `data_platform_payment_requisition_header_pdf_data`
+CREATE TABLE `data_platform_payment_requisition_header_doc_data`
 (
   `BusinessPartner`                int(12) NOT NULL,
   `PaymentDate`                    date NOT NULL,
   `PaymentRequisitionID`           int(6) NOT NULL,
   `DocType`                        varchar(20) NOT NULL,
+  `FileExtension`                  varchar(3) DEFAULT NULL,
   `DocVersionID`                   int(4) NOT NULL,
   `DocID`                          varchar(100) NOT NULL,
+  `DocIssuerBusinessPartner`       int(12) DEFAULT NULL,
+  `FilePath`                       varchar(1000) DEFAULT NULL,
   `FileName`                       varchar(200) DEFAULT NULL,
 
     PRIMARY KEY (`BusinessPartner`, `PaymentDate`, `PaymentRequisitionID`, `DocType`, `DocVersionID`, `DocID`),
